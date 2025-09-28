3ac4a6763be17a796b9debf42be9b108241e314a
DarkFox90 <carlospozuelos3@gmail.com>
2025-09-28 15:42:40 -0600

    terminada clase de combatiente

--- Combatiente.java
+++ Combatiente.java
@@ -1,4 +1,35 @@
 //modelo
 public class Combatiente {
-    
+    public String nombre;
+    public int puntosVida;
+    public int poderAtaque;
+
+    public Combatiente(String nombre, int puntosVida, int poderAtaque) {
+        this.nombre = nombre;
+        this.puntosVida = puntosVida;
+        this.poderAtaque = poderAtaque;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getPoderAtaque() {
+        return poderAtaque;
+    }
+
+    public int getPuntosVida() {
+        return puntosVida;
+    }
+
+    public void Atacar(Combatiente oponente) {
+        oponente.reducirVida(this.poderAtaque);
+    }
+
+    public void reducirVida(int cantidad) {
+        this.puntosVida -= cantidad;
+        if (this.puntosVida < 0) {
+            this.puntosVida = 0;
+        }
+    }
 }
