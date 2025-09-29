59953c3bcf5cfa4882ff9e46ad1afc9eb7562fe2
DarkFox90 <carlospozuelos3@gmail.com>
2025-09-28 18:10:56 -0600

    finalizado main y corregido errores

--- Main.java
+++ Main.java
@@ -33,8 +33,84 @@ public class Main {
         } else {
             heroe = new Heroe("jugador2", 90, 20, 5, "explorador", inventario);
             heroe.agregarItem(new Item("mini", "curacion", 30));
-            heroe.agregarItem(new Item("mini", "curacion", 30));
+            heroe.agregarItem(new Item("light armor", "aumentarDefensa", 2));
+            heroe.agregarItem(new Item("guantelete", "aumentarAtaque", 2));
         }
+
+        //creacion de villanos
+        int cantidadEnemigos = rnd.nextInt(3) + 1;
+        ArrayList<Villano> villanos = new ArrayList<>();
+
+        for (int i = 1; i <= cantidadEnemigos; i++) {
+            HabilidadEspecial hab = new HabilidadEspecial("fuego", 10 + rnd.nextInt(20)); //genera un numero aleatorio de poder para cada villano
+            Villano v = new Villano("Villano" + i, 80 + rnd.nextInt(41), 10 + rnd.nextInt(6), 10, "lacayo", hab);
+            Villano j = new Villano("Villano" + i, 150 + rnd.nextInt(41), 10 + rnd.nextInt(6), 10, "jefe", hab);
+            villanos.add(v);
+        }
+
+        //creacion de menus para logica de juego
+        MenuHeroe menuHeroe = new MenuHeroe();
+        MenuVillano menuVillano = new MenuVillano();
+
+        //iniciar la pelea
+        System.out.println("\nEmpieza la batalla entre heroes y villanos");
+        System.out.println(heroe.mostrarMensaje());
+        for (Villano v : villanos) {
+            System.out.println(v.mostrarMensaje());
+        }
+
+        //logica del combate
+        while (heroe.getPuntosVida() > 0 && !villanos.isEmpty()) {
+            System.out.println("\nestado de pelea");
+            System.out.println(heroe.getNombre() + " HP: " + heroe.getPuntosVida());
+            for(Villano v : villanos) {
+                System.out.println(v.getNombre() + " HP: " + v.getPuntosVida());
+            }
+
+            //manejo de turnos 
+            System.out.println("\nturno del heroe");
+            System.out.println(menuHeroe.mostrarMenu());
+            int opcionHeroe = sc.nextInt();
+
+            if (opcionHeroe == 1 && !villanos.isEmpty()) {
+                System.out.println("elige algun enemigo a atacar: ");
+                for (int i = 0; i < villanos.size(); i++) {
+                    System.out.println((i +1) + ". " + villanos.get(i).getNombre());
+                }
+
+                int indice = sc.nextInt() -1;
+                if (indice >= 0 && indice < villanos.size()) {
+                    menuHeroe.ejecutarOpcion(opcionHeroe, heroe, villanos.get(indice));
+                } else {
+                    menuHeroe.ejecutarOpcion(opcionHeroe, heroe, heroe);
+                }
+            }
+
+            // limpiar area de pelea
+            villanos.removeIf(v -> v.getPuntosVida() <= 0);
+            if(villanos.isEmpty()) {
+                System.out.println("Ganaron los heroes");
+                break;
+            }
+
+            //turno de villanos
+            for (Villano v : villanos) {
+                if (heroe.getPuntosVida() <= 0) {
+                    break;
+                }
+                System.out.println("\nturno de: " + v.getNombre() + ": ");
+                System.out.println(menuVillano.mostrarMenu());
+                int opcionVillano = sc.nextInt();
+                menuVillano.ejecutarOpcion(opcionVillano, v, heroe);
+            }
+
+            if (heroe.getPuntosVida() <= 0) {
+                System.out.println("perdieron los heroes");
+                break;
+            }
+        }
+
+        sc.close();
     }
 
 }
